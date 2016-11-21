a754ef04ef33b2748b0a0c6e9e50af8d696a25dd
giuliano <giu121@gmail.com>
2016-11-21 17:54:29 -0200

    terceiro commit

--- Ex210.java
+++ Ex210.java
@@ -2,4 +2,8 @@ public class TesteGit{
    public static void main (String args[]){
       System.out.println("Exemplo de teste usando Git");
    }
+   
+   public void primeiroMetodo() {
+	   System.out.println("primeiro");
+   }
 }
